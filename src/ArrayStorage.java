ec2ce5161570ee4fb3168b566aa77356b5609445
FlamieCyrex <wave-5@yandex.ru>
2023-10-03 21:40:38 +0300

    --method "getAll" was updated(size was changed by variable count)

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -46,7 +46,7 @@ public class ArrayStorage {
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return Arrays.copyOf(storage, size());
+        return Arrays.copyOf(storage, count);
     }
 
     int size() {
