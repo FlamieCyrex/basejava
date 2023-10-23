4fbc6e6730ce64995a14905f8654fad1032ec369
FlamieCyrex <wave-5@yandex.ru>
2023-10-23 12:35:28 +0300

    --method "getIndexOfResume" has been renamed to "getIndex" --method "save" has been fixed --access modifiers have been added to constants

--- src/com/javaops/webapp/storage/ArrayStorage.java
+++ src/com/javaops/webapp/storage/ArrayStorage.java
@@ -9,12 +9,12 @@ import java.util.Objects;
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    final int STORAGE_LIMIT = 10000;
-    final Resume[] storage = new Resume[STORAGE_LIMIT];
+    protected final int STORAGE_LIMIT = 10000;
+    private final Resume[] storage = new Resume[STORAGE_LIMIT];
 
     int size;
 
-    public int getIndexOfResume(String uuid) {
+    public int getIndex(String uuid) {
         for (int i = 0; i < size; i++) {
             if (Objects.equals(storage[i].uuid, uuid)) {
                 return i;
@@ -26,50 +26,50 @@ public class ArrayStorage {
     public void clear() {
         Arrays.fill(storage, 0, size, null);
         size = 0;
+        System.out.println("The storage has been cleared");
     }
 
-    public void update(Resume r){
-        int indexOfResume = getIndexOfResume(r.uuid);
-        if (indexOfResume>=0){
-                storage[indexOfResume] = r;
-                System.out.println("The resume has been updated");
-            }
-        else System.out.println("There is no such resume in storage for updating (uuid: " + r.uuid + ")");
+    public void update(Resume r) {
+        int index = getIndex(r.uuid);
+        if (index >= 0) {
+            storage[index] = r;
+            System.out.println("The resume has been updated");
+        } else {
+            System.out.println("There is no such resume in storage for updating (uuid: " + r.uuid + ")");
         }
+    }
 
     public void save(Resume r) {
-        if (size <= 9999) {
-            if (getIndexOfResume(r.uuid) >= 0) {
-                System.out.println("This resume already exists in the storage (uuid: " + r.uuid + ")");
-            }
-            else {
-                storage[size] = r;
-                size++;
-                System.out.println("The resume has been saved");
-            }
+        if (size >= STORAGE_LIMIT) {
+            System.out.println("There is no free space in storage for saving resume (uuid: " + r.uuid + ")");
+        } else if (getIndex(r.uuid) >= 0) {
+            System.out.println("This resume already exists in the storage (uuid: " + r.uuid + ")");
+        } else {
+            storage[size] = r;
+            size++;
+            System.out.println("The resume has been saved");
         }
-        else System.out.println("There is no free space in storage for saving resume (uuid: " + r.uuid + ")");
     }
 
 
     public Resume get(String uuid) {
-       int indexOfResume = getIndexOfResume(uuid);
-       if (indexOfResume>=0)
-           return storage[indexOfResume];
-       else
-        return null;
+        int index = getIndex(uuid);
+        if (index >= 0) {
+            return storage[index];
+        } else {
+            return null;
+        }
     }
 
     public void delete(String uuid) {
-        int indexOfResume = getIndexOfResume(uuid);
-        if (indexOfResume>=0){
-                storage[indexOfResume].uuid = storage[size - 1].uuid;
-                storage[size - 1] = null;
-                size--;
+        int index = getIndex(uuid);
+        if (index >= 0) {
+            storage[index].uuid = storage[size - 1].uuid;
+            storage[size - 1] = null;
+            size--;
             System.out.println("The resume has been deleted");
-            }
-        else
-            System.out.println("There is no such resume in storage (uuid: "+ uuid + ")");
+        } else
+            System.out.println("There is no such resume in storage (uuid: " + uuid + ")");
     }
 
     public Resume[] getAll() {
