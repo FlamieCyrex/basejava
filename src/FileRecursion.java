31a8f857813c23b56bb4da93116647c26340b983
FlamieCyrex <wave-5@yandex.ru>
2023-12-20 19:34:55 +0300

    --Recursion has been realized with indents

--- src/FileRecursion.java
+++ src/FileRecursion.java
@@ -2,20 +2,25 @@ import java.io.File;
 import java.util.Objects;
 
 public class FileRecursion {
-    public static void main(String[] args) {
-        File file = new File("C:\\Users\\flami\\OneDrive\\Рабочий стол\\Code\\JavaOPS\\basejava");
-        doRecursionFiles(file);
-    }
+    public static StringBuilder sb = new StringBuilder("");
 
     static void doRecursionFiles(File file) {
-        for (File file1 : Objects.requireNonNull(file.listFiles())) {
-            if (file1.isDirectory()) {
+        File[] files = file.listFiles();
+        for (File file1 : Objects.requireNonNull(files)) {
+            System.out.print(sb);
+            if (file1.isFile()) {
                 System.out.println(file1.getName());
+            } else if (file1.isDirectory()) {
+                System.out.println(file1.getName());
+                sb.append(" ");
                 doRecursionFiles(file1);
-            } else {
-                System.out.println("  " + file1.getName());
+                sb.delete(sb.length() - 1, sb.length());
             }
 
         }
+    }
+
+    public static void main(String[] args) {
+        doRecursionFiles(new File("C:\\Users\\flami\\OneDrive\\Рабочий стол\\Code\\JavaOPS\\basejava\\src"));
     }
 }
