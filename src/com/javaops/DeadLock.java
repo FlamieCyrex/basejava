ac261357c3c78624eb5755d788711ff431dec332
FlamieCyrex <wave-5@yandex.ru>
2024-01-23 19:27:01 +0300

    -Deadlock has been fixed

--- src/com/javaops/DeadLock.java
+++ src/com/javaops/DeadLock.java
@@ -16,35 +16,31 @@ public class DeadLock {
     public static class Thread1 extends Thread {
         @Override
         public void run() {
-            synchronized (object1) {
-                System.out.println(Thread.currentThread() + "is locking object1");
-                try {
-                    Thread.sleep(20);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-                synchronized (object2) {
-                    System.out.println(Thread.currentThread() + "is locking object1 and object2");
-                }
-            }
+            deadlock(object1, object2);
         }
     }
+
 
     public static class Thread2 extends Thread {
         @Override
         public void run() {
-            synchronized (object2) {
-                System.out.println(Thread.currentThread() + "is locking object2");
-                try {
-                    Thread.sleep(20);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-                synchronized (object1) {
-                    System.out.println(Thread.currentThread() + "is locking object1 and object2");
-                }
+            deadlock(object2, object1);
+        }
+    }
+
+    public static void deadlock(Object obj1, Object obj2) {
+        synchronized (obj1) {
+            System.out.println(Thread.currentThread() + "is locking on object");
+            try {
+                Thread.sleep(20);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            synchronized (obj2) {
+                System.out.println(Thread.currentThread() + "is locking object1 and object2");
             }
         }
     }
+
 
 }
